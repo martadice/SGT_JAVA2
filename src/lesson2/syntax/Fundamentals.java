dc875de8aca10cba7d0a4cd59251f441ab1bf4b7
Marta Dice <mmarta.dice@gmail.com>
2021-10-06 21:40:04 +0300

    lesson 2 added

---
+++ src/lesson2/syntax/Fundamentals.java
@@ -0,0 +1,38 @@
+package lesson2.syntax;
+
+/**
+ * comments about programme
+ */
+public class Fundamentals {
+    public static void main(String[] args) {
+//        COMMENTS
+
+        //we will write simple one line comments here
+        //and another line that starts with double slash
+
+        /*line 1 of the comment
+        line 2 of the comment
+        line 3
+        ...
+         */
+    }
+//      Documentation comments
+
+        // Documentation effectively describes methods, why method is needed, what are the method's paramaters,
+        // what it returns, used for all public methods
+    /**
+     * soemMethod should be in the following cases - when the method should be used
+     * @param param1 - name of the user
+     * @param param2 - number of smth
+     * @return
+     */
+    public String someMethod(String param1, int param2) {
+
+        return "something";
+    }
+ //
+/*-
+jghhchfchhgkchbb hbjbkbl  jbklj;jkhjgfdsrtyuiokl; kijhgfdghkhilj iujfgdghjhukjilk lgkjhgfdghjkj lkjhgfdghjkj lhkjghgfdghjk lkhjhgfhjk lkjghgfhjklhg lkjhgfhjkljhgfhjklhgfhjkljhgfyuiokjhgfghjkjhgfgghjhgfghjklijtyfdkjhgkjhgf
+ */
+
+}
